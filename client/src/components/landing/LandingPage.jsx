42c9c134f86d065b061979dadc5a9577022cc82b
Marsel <marselo3000@gmail.com>
2026-05-16 13:46:04 +0300

    Add CTABanner component with WhatsApp integration and footer links

--- client/src/components/landing/LandingPage.jsx
+++ client/src/components/landing/LandingPage.jsx
@@ -1,11 +1,13 @@
 import HeroSection from "./sections/hero/HeroSection";
 import CoursesGrid from "./sections/courses/CoursesGrid";
+import CTABanner from "./about/sections/cta/CTABanner";
 
 function LandingPage() {
   return (
     <>
       <HeroSection />
       <CoursesGrid />
+      <CTABanner />
     </>
   );
 }
