f62781ba45fdb174100e5e9037bf18bcfab91cee
Marsel <marselo3000@gmail.com>
2026-06-10 11:43:58 +0300

    style: format navigation links in Header component for improved readability

--- client/src/components/user/Header/Header.jsx
+++ client/src/components/user/Header/Header.jsx
@@ -10,11 +10,21 @@ function Header() {
 
       <nav className="portal-header__nav">
         <ul className="portal-header__nav-list">
-          <li><a href="#">Dashboard</a></li>
-          <li><a href="#">Course Catalog</a></li>
-          <li><a href="#">My Courses</a></li>
-          <li><a href="#">Learning Center</a></li>
-          <li><a href="#">Profile</a></li>
+          <li>
+            <a href="#">Dashboard</a>
+          </li>
+          <li>
+            <a href="#">Course Catalog</a>
+          </li>
+          <li>
+            <a href="#">My Courses</a>
+          </li>
+          <li>
+            <a href="#">Learning Center</a>
+          </li>
+          <li>
+            <a href="#">Profile</a>
+          </li>
         </ul>
       </nav>
     </header>
